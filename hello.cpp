7ededee8cacadfcac8b7182f45d2188667a71cc6
Ivan Konovalov <39829109+dex0806@users.noreply.github.com>
2022-09-26 07:26:48 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number1 = (0);
+	int number2 = (0);
+	
+	cin >> number1 >> '\n';
+	cin >> number2 >> '\n';
+    cout << "number1 + number2 = " << number1 + number2 << endl;
+	cout << "number1 - number2 = " << number1 - number2 << endl;
+    cout << "number1 * number2 = " << number1 * number2 << endl;
+    cout << "number1 / number2 = " << number1 / number2 << endl;
+    cout << "number1 % number2 = " << number1 % number2 << endl;
+    cout << endl;
+ 
+return 0;
+}
