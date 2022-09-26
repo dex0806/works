31500293604737c3c6340fab469377b25ee7824f
Ivan Konovalov <39829109+dex0806@users.noreply.github.com>
2022-09-26 07:27:26 +0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,8 +6,8 @@ int main()
 	int number1 = (0);
 	int number2 = (0);
 	
-	cin >> number1 >> '\n';
-	cin >> number2 >> '\n';
+	cin >> number1;
+	cin >> number2;
     cout << "number1 + number2 = " << number1 + number2 << endl;
 	cout << "number1 - number2 = " << number1 - number2 << endl;
     cout << "number1 * number2 = " << number1 * number2 << endl;
